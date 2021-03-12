0e38e1a970b8979fe3abe8d3486009ec5d2a0214
DDSoftDennie <dennie@ddsoft.be>
2021-03-12 15:30:33 +0100

    Added Domain and DataContext

---
+++ IkMag.Data/IkMagContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using IkMag.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace IkMag.Data
+{
+    public class IkMagContext : DbContext
+    {
+        public DbSet<Allowed> Allowed { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=IkMagDB.db;");
+        }
+
+
+
+    }
+}
---
+++ IkMag.Domain/Allowed.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IkMag.Domain
+{
+    public class Allowed
+    {
+        public int ID { get; set; }
+        public string CAT { get; set; }
+        public string AllowedRule { get; set; }
+        public string RestrictionRule { get; set; }
+        public bool HasRestrictions { get; set; }
+        public DateTime StartDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
